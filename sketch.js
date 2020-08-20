0fc8f54e6959d2971a6095adb52a65faa624fb25
Mohit289 <68867073+Mohit289@users.noreply.github.com>
2020-08-20 12:39:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+var ground
+var block1
+var block2
+var block3
+var block4
+var block5
+var block6
+var block7
+var block8
+var block9
+var block10
+var block11
+var block12
+var block13
+var block14
+var block15
+var block16
+
+var polygon
+function setup() {
+  createCanvas(800,400);
+  //level one
+  block1 = new Block(330,235,30,40);
+  block2 = new Block(330,235,30,40);
+  block3 = new Block(330,235,30,40);
+  block4 = new Block(330,235,30,40);
+  block5 = new Block(330,235,30,40);
+  block6 = new Block(330,235,30,40);
+  block7 = new Block(330,235,30,40);
+  //level two
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  //level three
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  //top
+  block16 = new Block(390,155,30,40);
+  
+  createSprite(400, 200, 50, 50);
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
